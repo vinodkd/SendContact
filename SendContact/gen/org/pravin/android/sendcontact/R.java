fec788adba06f1da87e5ff4cf3a632f4d5ba5a97
vinodkd <vinod@vinodkd.org>
2011-11-27 04:23:03 +0530

    modified design and implementation to pick contact instead of showing a contact list

--- SendContact/gen/org/pravin/android/sendcontact/R.java
+++ SendContact/gen/org/pravin/android/sendcontact/R.java
@@ -14,15 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int contactName=0x7f050001;
-        public static final int listContacts=0x7f050000;
+        public static final int pickContactButton=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int row=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int sendContactBySMS=0x7f040002;
     }
 }
